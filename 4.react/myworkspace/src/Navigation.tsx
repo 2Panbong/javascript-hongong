58037b5fd49a4ad8f297592d7a2ad69f2bb81f36
LEE HAK BONG <flamex2@naver.com>
2021-08-21 20:34:49 +0900

    0820 복습

---
+++ 4.react/myworkspace/src/Navigation.tsx
@@ -0,0 +1,27 @@
+const Navigation = () => {
+  return (
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/components">components</Link>
+      </li>
+      <li>
+        <Link to="/counter">Counter</Link>
+      </li>
+      <li>
+        <Link to="/calculator">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/generator">Generator</Link>
+      </li>
+      <li>
+        <Link to="/account-manager">AccountManager</Link>
+      </li>
+      <li>
+        <Link to="/bootstrap">BootStrap</Link>
+      </li>
+    </ul>
+  );
+};
